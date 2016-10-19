54c6874153825b0e8b8b6bc1249250c5739e496f
sds2001 <seth.d.schroeder@gmail.com>
2016-10-19 21:02:23 +0100

    create .txt file with hello world

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,24 @@
 // Copyright (c) 2016, sds2001. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
-
+import 'dart:async';
+import 'dart:io';
 
 
 main(List<String> arguments) {
   print('Hello world. This is my first Dart program!');
+  createFile("Hello world!");
+}
+
+Future createFile(String fileInput)async {
+  print('lol');
+  File file = new File('testfile.txt');
+
+  try {
+    await file.writeAsString(fileInput, mode: FileMode.WRITE);
+    print('Created file');
+
+  } catch(e){
+    print('Didnt Work. OOPS!');
+    print(e);
+  }
 }

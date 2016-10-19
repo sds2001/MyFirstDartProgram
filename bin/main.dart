d2123b1aa5d66d6754b4f7497a5cd9107364c3a8
sds2001 <seth.d.schroeder@gmail.com>
2016-10-19 20:40:13 +0100

    change message shown.

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,8 @@
 // Copyright (c) 2016, sds2001. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:MyFirstDartProgram/MyFirstDartProgram.dart' as MyFirstDartProgram;
+
 
 main(List<String> arguments) {
-  print('Hello world: ${MyFirstDartProgram.calculate()}!');
+  print('Hello world. This is my first Dart program!');
 }
